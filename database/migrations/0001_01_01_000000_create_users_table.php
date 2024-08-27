0e3fe968dd48918adda28c0e6bcb1f35a6cc5cec
Jawad Maqbool <Ahmedmaqbool@vizzweb.com>
2024-08-27 21:37:37 +0500

    added changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,10 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->enum('status', [
+                STATUS_ACTIVE,
+                STATUS_INACTIVE
+            ]);
             $table->timestamps();
         });
 
